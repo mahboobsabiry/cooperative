7d89c30a67aa0e786cde81295707a96ec02ebd8c
Mahboobulrahman Sabiry <mahboobulrahmansabiry@gmail.com>
2024-02-23 20:33:05 +0430

    Making some changes v2

--- database/migrations/2024_02_17_170620_create_experiences_table.php
+++ database/migrations/2024_02_17_170620_create_experiences_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('doc_number');
             $table->string('document')->nullable();
             $table->tinyInteger('status')->default(0);
-            $table->tinyInteger('user_status')->nullable();
             $table->text('info')->nullable();
             $table->timestamps();
 
