cb0f2a5ac7d12521d9c1c7b6a9f17cd3cb2ca4dc
mahboobsabiry <mahboobulrahmansabiry@gmail.com>
2024-05-25 12:44:07 +0430

    Working on Documents

---
+++ app/Http/Controllers/Admin/DocumentController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Document;
+use App\Models\File;
+use App\Models\Office\Position;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DocumentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (Auth::user()->isAdmin()) {
+            $position = null;
+            $documents = Document::all();
+        } else {
+            $position = Position::where('id', Auth::user()->employee->position_id)->firstOrFail();
+            $documents = Auth::user()->employee->position->documents;
+        }
+
+        return view('admin.documents.index', compact('position', 'documents'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $position = Position::where('id', Auth::user()->employee->position_id)->firstOrFail();
+        return view('admin.documents.create', compact('position'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'subject'       => 'required',
+            'doc_number'    => 'required',
+            'doc_date'      => 'required',
+            'appendices'    => 'required',
+            'document'      => 'required'
+        ]);
+
+        $position = Position::where('id', Auth::user()->employee->position_id)->firstOrFail();
+
+        $document           = new Document();
+        $document->position_id = $position->id;
+        $document->type     = $request->type;
+        $document->doc_type = $request->doc_type;
+        $document->subject  = $request->subject;
+        $document->doc_number   = $request->doc_number;
+        $document->doc_date     = $request->doc_date;
+        $document->appendices   = $request->appendices;
+        $document->status   = 1;
+        $document->info     = $request->info;
+        $document->save();
+
+        if ($request->hasFile('document')) {
+            // File
+            // $file = $request->file('document');
+            foreach ($request->file('document') as $item) {
+                // New Document
+                $f = new File();
+                $fileName = 'doc-file-' . time() . '.' . $item->getClientOriginalExtension();
+                $item->storeAs('documents/files', $fileName, 'public');
+                $f->path   = $fileName;
+                $document->files()->save($f);
+            }
+        }
+
+        return redirect()->route('admin.documents.index')->with([
+            'message'   => 'مکتوب موفقانه ذخیره گردید.',
+            'alertType' => 'success'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $document = Document::find($id);
+        $position = $document->position;
+        return view('admin.documents.show', compact('document', 'position'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $document = Document::find($id);
+        $position = $document->position;
+        return view('admin.documents.edit', compact('document', 'position'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $document = Document::find($id);
+        $request->validate([
+            'subject'       => 'required',
+            'doc_number'    => 'required',
+            'doc_date'      => 'required',
+            'appendices'    => 'required',
+            'document'      => 'required'
+        ]);
+
+        $position = Position::where('id', Auth::user()->employee->position_id)->firstOrFail();
+
+        $document->position_id = $position->id;
+        $document->type     = $request->type;
+        $document->doc_type = $request->doc_type;
+        $document->subject  = $request->subject;
+        $document->doc_number   = $request->doc_number;
+        $document->doc_date     = $request->doc_date;
+        $document->appendices   = $request->appendices;
+        $document->status   = 1;
+        $document->info     = $request->info;
+        $document->save();
+
+        if ($request->hasFile('document')) {
+            // File
+            // $file = $request->file('document');
+            foreach ($request->file('document') as $item) {
+                // New Document
+                $f = new File();
+                $fileName = 'doc-file-' . time() . '.' . $item->getClientOriginalExtension();
+                $item->storeAs('documents/files', $fileName, 'public');
+                $f->path   = $fileName;
+                $document->files()->save($f);
+            }
+        }
+
+        return redirect()->route('admin.documents.index')->with([
+            'message'   => 'مکتوب موفقانه بروزرسانی گردید.',
+            'alertType' => 'info'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $document = Document::find($id);
+
+        $document->delete();
+
+        return redirect()->route('admin.documents.index')->with([
+            'message'   => 'مکتوب موفقانه حذف گردید.',
+            'alertType' => 'danger'
+        ]);
+    }
+}
