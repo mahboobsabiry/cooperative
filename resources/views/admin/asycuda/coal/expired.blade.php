78d5f22b671b8a44dffcffe7dd3a6b7131f9f536
Mahboobulrahman Sabiry <mahboobulrahmansabiry@gmail.com>
2024-05-30 08:25:15 +0430

    Working on COAL Expired

--- resources/views/admin/asycuda/coal/expired.blade.php
+++ resources/views/admin/asycuda/coal/expired.blade.php
@@ -53,19 +53,19 @@
         <!-- Data Table -->
         <div class="row">
             <div class="col-lg-12">
+                <!-- Success Message -->
+                @include('admin.inc.alerts')
+
                 <!-- Table Card -->
-                <div class="card custom-card main-content-body-profile">
+                <div class="card mb-2">
+                    <div class="card-header tx-15 tx-bold">
+                        جواز فعالیت ختم شده شرکت ها ({{ count($coal) }})
+                    </div>
+
                     <!-- Table Card Body -->
-                    <div class="card-body tab-content h-100">
-                        <!-- Success Message -->
-                        @include('admin.inc.alerts')
-
+                    <div class="card-body">
                         <!-- Employees -->
-                        <div class="tab-pane active">
-                            <div class="main-content-label tx-13 mg-b-20">
-                                جواز فعالیت ختم شده شرکت ها ({{ count($coal) }})
-                            </div>
-
+                        <div class="">
                             <!-- Table -->
                             <div class="table-responsive">
                                 <table class="table table-striped table-bordered dataTable export-table border-top key-buttons display text-nowrap w-100" style="width: 100%;">
