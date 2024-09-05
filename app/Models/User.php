1036bbd73f4b7db66bd471298cd608b4fe2c58f6
Mahboobulrahman Sabiry <mahboobulrahmansabiry@gmail.com>
2024-09-05 11:45:59 +0430

    Working on User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -103,36 +103,6 @@ class User extends Authenticatable
         return $this->is_admin == 1;
     }
 
-    // Head - Riyasat
-    public function head()
-    {
-        return $this->place == 0;
-    }
-
-    // Border
-    public function border()
-    {
-        return $this->place == 1;
-    }
-
-    // Airport
-    public function airport()
-    {
-        return $this->place == 2;
-    }
-
-    // Nayeb ABAD
-    public function nAbad()
-    {
-        return $this->place == 3;
-    }
-
-    // Muraqeban Sayar
-    public function mSayar()
-    {
-        return $this->place == 4;
-    }
-
     // Has Many Assurance
     public function assurances() : HasMany
     {
