da15e779e4e486f4d92428cfdddd33f5de95adbb
Mahboobulrahman Sabiry <mahboobulrahmansabiry@gmail.com>
2024-09-06 16:45:32 +0430

    Working on Employee Migration

--- database/migrations/2023_12_13_194115_create_employees_table.php
+++ database/migrations/2023_12_13_194115_create_employees_table.php
@@ -39,7 +39,7 @@ return new class extends Migration
             $table->string('current_district');
             $table->string('introducer')->nullable();
             $table->string('signature')->nullable();
-            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('status')->default(1);
             $table->string('info')->nullable();
 
             // Duty
