af47df5853f374f8bb2c10ffecd783ab9de55d7b
Mahboobulrahman Sabiry <mahboobulrahmansabiry@gmail.com>
2024-11-28 16:25:24 +0430

    Working on Website

---
+++ resources/views/website/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.website.master')
+
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('admin.dashboard');
+    return view('website.index');
 })->name('index');
 
 Route::get('/optimize', function () {
